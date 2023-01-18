a497bd185623dfea28ca57cc00ef03e4bd6f95e7
Matthew Leland <mleland18@apu.edu>
2023-01-17 16:22:53 -0800

    express server and initial route

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('Hello Stathlete')
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+const app = express()
+
+app.get('/api/favorites', (req, res) => {
+  res.status(200).json({ message: 'Get Favorites'})
+})
+
+const port = process.env.PORT || 5001
+app.listen(port, () => console.log(`Server listening on port ${port}...`))
