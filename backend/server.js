25ceaaa8e047f010fff13ef9c3f661ab7755ab3f
Joshua Lee <joshlee1117@yahoo.com>
2023-01-25 21:32:24 -0800

    User Routes and Controller

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/favorites', require('./routes/favRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.use(errorHandler)
 
