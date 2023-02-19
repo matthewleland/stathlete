71003038e7ec99674a27286291507221e965fee4
Matthew Leland <mjleland4@gmail.com>
2023-02-18 23:55:59 -0800

    tailwind and daisyui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,21 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import Header from './components/Header'
+import Navbar from './components/layout/Navbar'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Search from './pages/Search'
 
 function App() {
   return (
     <>
         <Router>
           <div className="container">
-            <Header />
+            <Navbar />
             <Routes>
               <Route path='/' element={<Dashboard />} />
+              <Route path='/search' element={<Search />} />
               <Route path='/login' element={<Login />} />
               <Route path='/register' element={<Register />} />
             </Routes>
---
+++ frontend/src/components/layout/Navbar.jsx
@@ -0,0 +1,77 @@
+import { Link, useNavigate } from 'react-router-dom'
+import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
+import { GiBasketballBasket } from 'react-icons/gi'
+import { useSelector, useDispatch } from 'react-redux'
+import { logout, reset } from '../../features/auth/authSlice'
+import PropTypes from 'prop-types'
+
+function Navbar({ title }) {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { user } = useSelector((state) => state.auth)
+
+  const onLogout = () => {
+    dispatch(logout())
+    dispatch(reset())
+    navigate('/')
+  }
+
+  return (
+    <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
+      <div className='container mx-auto'>
+        <div className='flex-none px-2 mx-2'>
+          <GiBasketballBasket className='inline pr-2 text-5xl' />
+          <Link to='/' className='text-lg font-bold align-middle'>
+            {title}
+          </Link>
+        </div>
+
+        <div className='flex-1 px-2 mx-2'>
+          <div className='flex justify-end'>
+            <ul className="menu menu-horizontal px-1">
+              <li>
+                <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>
+                  Dashboard
+                </Link>
+              </li>
+              <li>
+                <Link to='/search' className='btn btn-ghost btn-sm rounded-btn'>
+                  Search
+                </Link>
+              </li>
+              <li tabIndex={0}>
+                <Link to='/profile' className='btn btn-ghost btn-sm rounded-btn'>
+                  {user.name}
+                </Link>
+                <ul className="p-2 bg-base-100">
+                  <li>
+                    <Link to='/profile' className='btn btn-ghost btn-sm rounded-btn'>
+                      Profile
+                    </Link>
+                    <Link to='/favorites' className='btn btn-ghost btn-sm rounded-btn'>
+                      Favorites
+                    </Link>
+                  </li>
+                  <li>
+                    <button className="btn btn-ghost btn-sm rounded-btn">Log Out</button>
+                  </li>
+                </ul>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+Navbar.defaultProps = {
+  title: 'Stathlete',
+}
+
+Navbar.propTypes = {
+  title: PropTypes.string,
+}
+
+export default Navbar
