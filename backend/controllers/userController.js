c663de3a9ec9740b2c840720150e6b1d759e2e36
Joshua Lee <joshlee1117@yahoo.com>
2023-03-13 16:55:23 -0700

    Secure Password Reset - updated userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,15 +22,17 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error('User already exists')
     }
 
-    // Hash password
+    // (OLD) Hash password - see userModel.js for hashing
+    /* 
     const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(password, salt)
+    */
 
     // Create user
     const user = await User.create({
         name,
         email,
-        password: hashedPassword
+        password //: hashedPassword
     })
 
     if (user) {
