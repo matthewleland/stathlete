753d98aaef7119539bbcc9e9e3318cccfd1b79c6
Joshua Lee <joshlee1117@yahoo.com>
2023-03-13 11:11:48 -0700

    Secure Password Reset - Fixed bcryptjs import

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const bcryptSalt = process.env.BCRYPT_SALT;
 
 const userSchema = mongoose.Schema({
