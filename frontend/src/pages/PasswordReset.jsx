06efbe6a83cc5434ffa80fb3b8d40b3261bc3413
Joshua Lee <jslee18@apu.edu>
2023-03-13 23:32:06 -0700

    Secure Password Reset - updated pwd reset page

--- frontend/src/pages/PasswordReset.jsx
+++ frontend/src/pages/PasswordReset.jsx
@@ -2,7 +2,43 @@
 
 function PasswordReset() {
     return (
-        <p>Password Reset Page</p>
+        <>
+            <div>
+                <label for="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                <input 
+                    type="text" 
+                    id="email" 
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                    placeholder="Enter your email..." 
+                    required>
+                </input>
+            </div>
+            <div>
+                <label for="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Password</label>
+                <input 
+                    type="text" 
+                    id="new_password" 
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                    placeholder="Enter your new password..." 
+                    required>
+
+                </input>
+            </div>
+            <div>
+                <label for="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm New Password</label>
+                <input 
+                    type="text" 
+                    id="confirm_new_password" 
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                    placeholder="Re-enter your new password..." 
+                    required>
+                </input>
+            </div>
+            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Submit
+            </button>
+        </>
+        
     )
 }
 
