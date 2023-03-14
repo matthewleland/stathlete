264db287baf9651882ec4279dfd389a71b8e8347
Joshua Lee <jslee18@apu.edu>
2023-03-13 22:31:20 -0700

    Secure Password Reset - reset password function + routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -88,7 +88,7 @@ const generateToken = (id) => {
 }
 
 // TODO
-// @desc    Reset user password
+// @desc    Request to reset user password
 const requestPasswordReset = asyncHandler(async (email) => {
     const user = await User.findOne({ email })
 
@@ -116,9 +116,40 @@ const requestPasswordReset = asyncHandler(async (email) => {
     return link;
 })
 
+// TODO
+// @desc    Reset user password
+const resetPassword = asyncHandler(async (userId, token, password) => {
+    let passwordResetToken = await Token.findOne({ userId });
+    if (!passwordResetToken) {
+      throw new Error("Invalid or expired password reset token");
+    }
+    const isValid = await bcrypt.compare(token, passwordResetToken.token);
+    if (!isValid) {
+      throw new Error("Invalid or expired password reset token");
+    }
+    const hash = await bcrypt.hash(password, Number(bcryptSalt));
+    await User.updateOne(
+      { _id: userId },
+      { $set: { password: hash } },
+      { new: true }
+    );
+    const user = await User.findById({ _id: userId });
+    sendEmail(
+      user.email,
+      "Password Reset Successfully",
+      {
+        name: user.name,
+      },
+      "../Utils/Emails/templates/resetPassword.handlebars"
+    );
+    await passwordResetToken.deleteOne();
+    return true;
+});
+
 module.exports = {
     registerUser,
     loginUser,
     getMe,
     requestPasswordReset,
+    resetPassword,
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { registerUser, loginUser, getMe } = require('../controllers/userController')
+const { registerUser, loginUser, getMe, requestPasswordReset, resetPassword } = require('../controllers/userController')
 const { protect } = require('../middleware/authMiddleware')
 
 router.post('/', registerUser)
 router.post('/login', loginUser)
 router.get('/me', protect, getMe)
+router.post('/passwordReset', requestPasswordReset, resetPassword)
 
 module.exports = router
