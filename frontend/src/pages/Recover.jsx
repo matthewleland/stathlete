bfdce7df47dc99f702fff9d2cf46790951fea870
Joshua Lee <jslee18@apu.edu>
2023-04-18 09:19:53 -0700

    Password Reset - Updated Recover.jsx

--- frontend/src/pages/Recover.jsx
+++ frontend/src/pages/Recover.jsx
@@ -1,7 +1,40 @@
+import { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { resetPassword } from '../features/auth/authSlice'
+
 function Recover() {
+
+  const [formData, setFormData] = useState({
+    email: '',
+    newPassword: '',
+    confirmPassword: ''
+  })
+
+  const { email, newPassword, confirmPassword } = formData
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    const userData = {
+      email,
+      newPassword
+    }
+
+    dispatch(resetPassword())
+  }
+
   return (
     <>
-      <form>
+      <form
+        onSubmit={onSubmit}
+      >
         <div className="grid gap-6 mb-6 mx-6 md:grid-cols-1">
           <div>
             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -12,6 +45,8 @@ function Recover() {
               id="email"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Enter your email..."
+              value={email}
+              onChange={onChange}
               required
             ></input>
           </div>
@@ -24,6 +59,8 @@ function Recover() {
               id="new_password"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Enter your new password..."
+              value={newPassword}
+              onChange={onChange}
               required
             ></input>
           </div>
@@ -36,6 +73,8 @@ function Recover() {
               id="confirm_new_password"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Re-enter your new password..."
+              value={confirmPassword}
+              onChange={onChange}
               required
             ></input>
           </div>
